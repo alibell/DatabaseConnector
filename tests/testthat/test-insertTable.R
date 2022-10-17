c9cc2547c652b38058da5abaa9dc1fe0c05f20df
Ali <contact@alibellamine.me>
2022-10-17 13:06:43 +0200

    Adding unit test for insert-table with spark

--- tests/testthat/test-insertTable.R
+++ tests/testthat/test-insertTable.R
@@ -35,179 +35,179 @@ test_that("insertTable", {
   data$big_ints[7] <- NA
   data$big_ints[8] <- 3.3043e+10
 
-  # # Postgresql --------------------------------------
-  # details <- createConnectionDetails(
-  #   dbms = "postgresql",
-  #   user = Sys.getenv("CDM5_POSTGRESQL_USER"),
-  #   password = URLdecode(Sys.getenv("CDM5_POSTGRESQL_PASSWORD")),
-  #   server = Sys.getenv("CDM5_POSTGRESQL_SERVER")
-  # )
-  # connection <- connect(details)
-  # insertTable(
-  #   connection = connection,
-  #   tableName = "temp",
-  #   data = data,
-  #   createTable = TRUE,
-  #   tempTable = TRUE
-  # )
+  # Postgresql --------------------------------------
+  details <- createConnectionDetails(
+    dbms = "postgresql",
+    user = Sys.getenv("CDM5_POSTGRESQL_USER"),
+    password = URLdecode(Sys.getenv("CDM5_POSTGRESQL_PASSWORD")),
+    server = Sys.getenv("CDM5_POSTGRESQL_SERVER")
+  )
+  connection <- connect(details)
+  insertTable(
+    connection = connection,
+    tableName = "temp",
+    data = data,
+    createTable = TRUE,
+    tempTable = TRUE
+  )
 
-  # # Check data on server is same as local
-  # data2 <- querySql(connection, "SELECT * FROM temp", integer64AsNumeric = FALSE)
-  # names(data2) <- tolower(names(data2))
-  # expect_equal(data, data2, check.attributes = FALSE)
+  # Check data on server is same as local
+  data2 <- querySql(connection, "SELECT * FROM temp", integer64AsNumeric = FALSE)
+  names(data2) <- tolower(names(data2))
+  expect_equal(data, data2, check.attributes = FALSE)
 
-  # # Check data types
-  # res <- dbSendQuery(connection, "SELECT * FROM temp")
-  # columnInfo <- dbColumnInfo(res)
-  # dbClearResult(res)
-  # expect_equal(as.character(columnInfo$field.type), c("date", "timestamp", "int4", "numeric", "varchar", "int8"))
+  # Check data types
+  res <- dbSendQuery(connection, "SELECT * FROM temp")
+  columnInfo <- dbColumnInfo(res)
+  dbClearResult(res)
+  expect_equal(as.character(columnInfo$field.type), c("date", "timestamp", "int4", "numeric", "varchar", "int8"))
 
-  # disconnect(connection)
-
-
-  # # SQL Server ----------------------------------------------
-  # details <- createConnectionDetails(
-  #   dbms = "sql server",
-  #   user = Sys.getenv("CDM5_SQL_SERVER_USER"),
-  #   password = URLdecode(Sys.getenv("CDM5_SQL_SERVER_PASSWORD")),
-  #   server = Sys.getenv("CDM5_SQL_SERVER_SERVER")
-  # )
-  # connection <- connect(details)
-  # insertTable(
-  #   connection = connection,
-  #   tableName = "#temp",
-  #   data = data,
-  #   createTable = TRUE,
-  #   tempTable = TRUE
-  # )
-
-  # # Check data on server is same as local
-  # data2 <- querySql(connection, "SELECT * FROM #temp", integer64AsNumeric = FALSE)
-  # names(data2) <- tolower(names(data2))
-  # data <- data[order(data$person_id), ]
-  # data2 <- data2[order(data2$person_id), ]
-  # row.names(data) <- NULL
-  # row.names(data2) <- NULL
-  # expect_equal(data, data2, check.attributes = FALSE)
-
-  # # Check data types
-  # res <- dbSendQuery(connection, "SELECT * FROM #temp")
-  # columnInfo <- dbColumnInfo(res)
-  # dbClearResult(res)
-  # expect_equal(as.character(columnInfo$field.type), c("date", "datetime2", "int", "float", "varchar", "bigint"))
-
-  # disconnect(connection)
+  disconnect(connection)
 
 
-  # # Oracle -----------------------------------------
-  # details <- createConnectionDetails(
-  #   dbms = "oracle",
-  #   user = Sys.getenv("CDM5_ORACLE_USER"),
-  #   password = URLdecode(Sys.getenv("CDM5_ORACLE_PASSWORD")),
-  #   server = Sys.getenv("CDM5_ORACLE_SERVER")
-  # )
-  # schema <- Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA")
-  # connection <- connect(details)
-  # insertTable(
-  #   connection = connection,
-  #   databaseSchema = schema,
-  #   tableName = "temp",
-  #   data = data,
-  #   createTable = TRUE,
-  #   tempTable = FALSE
-  # )
+  # SQL Server ----------------------------------------------
+  details <- createConnectionDetails(
+    dbms = "sql server",
+    user = Sys.getenv("CDM5_SQL_SERVER_USER"),
+    password = URLdecode(Sys.getenv("CDM5_SQL_SERVER_PASSWORD")),
+    server = Sys.getenv("CDM5_SQL_SERVER_SERVER")
+  )
+  connection <- connect(details)
+  insertTable(
+    connection = connection,
+    tableName = "#temp",
+    data = data,
+    createTable = TRUE,
+    tempTable = TRUE
+  )
+
+  # Check data on server is same as local
+  data2 <- querySql(connection, "SELECT * FROM #temp", integer64AsNumeric = FALSE)
+  names(data2) <- tolower(names(data2))
+  data <- data[order(data$person_id), ]
+  data2 <- data2[order(data2$person_id), ]
+  row.names(data) <- NULL
+  row.names(data2) <- NULL
+  expect_equal(data, data2, check.attributes = FALSE)
+
+  # Check data types
+  res <- dbSendQuery(connection, "SELECT * FROM #temp")
+  columnInfo <- dbColumnInfo(res)
+  dbClearResult(res)
+  expect_equal(as.character(columnInfo$field.type), c("date", "datetime2", "int", "float", "varchar", "bigint"))
+
+  disconnect(connection)
 
 
-  # # lowLevelQuerySql(connection, "SELECT * FROM all_tab_columns WHERE owner = 'OHDSI' AND table_name = 'TEMP'")
+  # Oracle -----------------------------------------
+  details <- createConnectionDetails(
+    dbms = "oracle",
+    user = Sys.getenv("CDM5_ORACLE_USER"),
+    password = URLdecode(Sys.getenv("CDM5_ORACLE_PASSWORD")),
+    server = Sys.getenv("CDM5_ORACLE_SERVER")
+  )
+  schema <- Sys.getenv("CDM5_ORACLE_OHDSI_SCHEMA")
+  connection <- connect(details)
+  insertTable(
+    connection = connection,
+    databaseSchema = schema,
+    tableName = "temp",
+    data = data,
+    createTable = TRUE,
+    tempTable = FALSE
+  )
 
-  # # Check data on server is same as local
-  # sql <- SqlRender::render("SELECT * FROM @schema.temp", schema = schema)
-  # data2 <- querySql(connection, sql, integer64AsNumeric = FALSE)
-  # names(data2) <- tolower(names(data2))
-  # data <- data[order(data$person_id), ]
-  # data2 <- data2[order(data2$person_id), ]
-  # row.names(data) <- NULL
-  # row.names(data2) <- NULL
-  # expect_equal(data, data2, check.attributes = FALSE)
 
-  # # Check data types
-  # res <- dbSendQuery(connection, sprintf("SELECT * FROM %s.temp", schema))
-  # columnInfo <- dbColumnInfo(res)
-  # dbClearResult(res)
-  # expect_equal(as.character(columnInfo$field.type), c("DATE", "TIMESTAMP", "NUMBER", "NUMBER", "VARCHAR2", "NUMBER"))
+  # lowLevelQuerySql(connection, "SELECT * FROM all_tab_columns WHERE owner = 'OHDSI' AND table_name = 'TEMP'")
 
-  # disconnect(connection)
+  # Check data on server is same as local
+  sql <- SqlRender::render("SELECT * FROM @schema.temp", schema = schema)
+  data2 <- querySql(connection, sql, integer64AsNumeric = FALSE)
+  names(data2) <- tolower(names(data2))
+  data <- data[order(data$person_id), ]
+  data2 <- data2[order(data2$person_id), ]
+  row.names(data) <- NULL
+  row.names(data2) <- NULL
+  expect_equal(data, data2, check.attributes = FALSE)
 
-  # # SQLite ---------------------------------------------------------------
-  # dbFile <- tempfile()
-  # details <- createConnectionDetails(
-  #   dbms = "sqlite",
-  #   server = dbFile
-  # )
-  # connection <- connect(details)
-  # insertTable(
-  #   connection = connection,
-  #   tableName = "temp",
-  #   data = data,
-  #   createTable = TRUE,
-  #   tempTable = FALSE
-  # )
+  # Check data types
+  res <- dbSendQuery(connection, sprintf("SELECT * FROM %s.temp", schema))
+  columnInfo <- dbColumnInfo(res)
+  dbClearResult(res)
+  expect_equal(as.character(columnInfo$field.type), c("DATE", "TIMESTAMP", "NUMBER", "NUMBER", "VARCHAR2", "NUMBER"))
 
-  # # Check data on server is same as local
-  # data2 <- querySql(connection, "SELECT * FROM temp", integer64AsNumeric = FALSE)
-  # names(data2) <- tolower(names(data2))
-  # data <- data[order(data$person_id), ]
-  # data2 <- data2[order(data2$person_id), ]
-  # row.names(data) <- NULL
-  # row.names(data2) <- NULL
+  disconnect(connection)
 
-  # attr(data$some_datetime, "tzone") <- NULL
-  # attr(data2$some_datetime, "tzone") <- NULL
-  # expect_equal(data, data2, check.attributes = FALSE)
+  # SQLite ---------------------------------------------------------------
+  dbFile <- tempfile()
+  details <- createConnectionDetails(
+    dbms = "sqlite",
+    server = dbFile
+  )
+  connection <- connect(details)
+  insertTable(
+    connection = connection,
+    tableName = "temp",
+    data = data,
+    createTable = TRUE,
+    tempTable = FALSE
+  )
 
-  # # Check data types
-  # res <- dbSendQuery(connection, "SELECT * FROM temp")
-  # columnInfo <- dbColumnInfo(res)
-  # dbClearResult(res)
-  # expect_equal(as.character(columnInfo$type), c("double", "double", "integer", "double", "character", "double"))
+  # Check data on server is same as local
+  data2 <- querySql(connection, "SELECT * FROM temp", integer64AsNumeric = FALSE)
+  names(data2) <- tolower(names(data2))
+  data <- data[order(data$person_id), ]
+  data2 <- data2[order(data2$person_id), ]
+  row.names(data) <- NULL
+  row.names(data2) <- NULL
 
-  # disconnect(connection)
-  # unlink(dbFile)
+  attr(data$some_datetime, "tzone") <- NULL
+  attr(data2$some_datetime, "tzone") <- NULL
+  expect_equal(data, data2, check.attributes = FALSE)
 
-  # # RedShift --------------------------------------
-  # details <- createConnectionDetails(
-  #   dbms = "redshift",
-  #   user = Sys.getenv("CDM5_REDSHIFT_USER"),
-  #   password = URLdecode(Sys.getenv("CDM5_REDSHIFT_PASSWORD")),
-  #   server = Sys.getenv("CDM5_REDSHIFT_SERVER")
-  # )
-  # connection <- connect(details)
+  # Check data types
+  res <- dbSendQuery(connection, "SELECT * FROM temp")
+  columnInfo <- dbColumnInfo(res)
+  dbClearResult(res)
+  expect_equal(as.character(columnInfo$type), c("double", "double", "integer", "double", "character", "double"))
 
-  # # Use subset of data because inserts on RedShift are slow:
-  # subset <- data[1:10, ]
+  disconnect(connection)
+  unlink(dbFile)
 
-  # insertTable(
-  #   connection = connection,
-  #   tableName = "temp",
-  #   data = subset,
-  #   createTable = TRUE,
-  #   tempTable = TRUE
-  # )
+  # RedShift --------------------------------------
+  details <- createConnectionDetails(
+    dbms = "redshift",
+    user = Sys.getenv("CDM5_REDSHIFT_USER"),
+    password = URLdecode(Sys.getenv("CDM5_REDSHIFT_PASSWORD")),
+    server = Sys.getenv("CDM5_REDSHIFT_SERVER")
+  )
+  connection <- connect(details)
 
-  # # Check data on server is same as local
-  # data2 <- querySql(connection, "SELECT * FROM temp", integer64AsNumeric = FALSE)
-  # names(data2) <- tolower(names(data2))
-  # subset <- subset[order(subset$person_id), ]
-  # data2 <- data2[order(data2$person_id), ]
-  # expect_equal(subset, data2, check.attributes = FALSE)
+  # Use subset of data because inserts on RedShift are slow:
+  subset <- data[1:10, ]
 
-  # # Check data types
-  # res <- dbSendQuery(connection, "SELECT * FROM temp")
-  # columnInfo <- dbColumnInfo(res)
-  # dbClearResult(res)
-  # expect_equal(as.character(columnInfo$field.type), c("date", "timestamp", "int4", "float8", "varchar", "int8"))
+  insertTable(
+    connection = connection,
+    tableName = "temp",
+    data = subset,
+    createTable = TRUE,
+    tempTable = TRUE
+  )
 
-  # disconnect(connection)
+  # Check data on server is same as local
+  data2 <- querySql(connection, "SELECT * FROM temp", integer64AsNumeric = FALSE)
+  names(data2) <- tolower(names(data2))
+  subset <- subset[order(subset$person_id), ]
+  data2 <- data2[order(data2$person_id), ]
+  expect_equal(subset, data2, check.attributes = FALSE)
+
+  # Check data types
+  res <- dbSendQuery(connection, "SELECT * FROM temp")
+  columnInfo <- dbColumnInfo(res)
+  dbClearResult(res)
+  expect_equal(as.character(columnInfo$field.type), c("date", "timestamp", "int4", "float8", "varchar", "int8"))
+
+  disconnect(connection)
 
   # Spark ---------------------------------------------------------------
   sc <- loadSpark()
@@ -241,7 +241,7 @@ test_that("insertTable", {
   res <- dbSendQuery(connection, "SELECT * FROM temp")
   columnInfo <- dbColumnInfo(res)
   dbClearResult(res)
-  expect_equal(as.character(columnInfo$type), c("double", "double", "integer", "double", "character", "double"))
+  expect_equal(as.character(columnInfo$type), c("Date", "POSIXct", "integer", "numeric", "character", "integer64"))
 
   disconnect(connection)
 })
